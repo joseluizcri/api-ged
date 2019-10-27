9482bf2bac25859efe89786f0e53bbd32c1e1fe5
José Luiz <junior.joseluizrosa@gmail.com>
2019-10-27 02:10:03 -0200

    Criando projeto

---
+++ src/main/java/br/com/ged/apiged/ApiGedApplication.java
@@ -0,0 +1,13 @@
+package br.com.ged.apiged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ged/apiged/ApiGedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ged.apiged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
